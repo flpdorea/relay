c0c85560e0aa82c719ef0fe8880522c194fdc021
Felipe Dórea <felipeldorea@gmail.com>
2026-02-22 22:46:12 -0300

    feat(strategies): add barrel export for clean imports

---
+++ src/strategies/index.ts
@@ -0,0 +1,6 @@
+export type { LoadBalancingStrategy } from './types.js';
+export { RoundRobin } from './RoundRobin.js';
+export { LeastConnections } from './LeastConnections.js';
+export { Weighted } from './Weighted.js';
+export { Random } from './Random.js';
+export { StrategyFactory } from './StrategyFactory.js';
